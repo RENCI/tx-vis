921cab7f8ad99f52ea63789deec9bc7700063070
Shruti <sraj@renci.org>
2019-09-13 09:58:55 -0400

    dispatcher.py

--- pdsphenotypemapping/dispatcher.py
+++ pdsphenotypemapping/dispatcher.py
@@ -1,27 +1,36 @@
 import json
 import os 
 
-def loadFHIR(input_file):
+# function for loading patient record file(s)
+
+def loadFHIR(input_file): 
     return json.load(input_file)
 
-
-def lookupFHIR(input_dir, patient_id, codes): # [{"system":"", "code":""}]
+def lookupFHIR(input_dir, patient_id, codes): # 'codes' parameter is a list of [{"system":"", "code":""}]
     records = []
     for root, _, fnames in os.walk(input_dir, topdown=True):
         for f in fnames:
             resc = loadFHIR(os.path.join(root, f))
-            for c in codes:
+
+            for c in codes:  
                 system = c["system"]
                 code = c["code"]
                 if system == "http://loinc.org":
                     resc_type = "Observation"
                 elif system == "http://hl7.org/fhir/sid/icd-10":
                     resc_type = "Condition"
-            
+                elif system == "http://hl7.org/fhir/sid/icd-9-cm":
+                    resc_type = "Condition"
+                else:
+                    print ("Unknown resource type")
+
                 if resc["resourceType"] == resc_type:
-                    for c2 in resc["code"]["coding"]:
-                        if c2["system"] == system and c2["code"] == code:
-                            records.append(resc)
+                    for c2 in resc["code"]["coding"]: # compares system and diagnosis codes
+                        if c2["system"] == system and c2["code"] == code:  
+                            for p in patient_id: # compares patient_ids
+                                pid = subject[reference].split("/")
+                                if p == pid[1]:
+                                    records.append(resc)
                             
                     
                 
