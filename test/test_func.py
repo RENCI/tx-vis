45b2076c0d7deb01d22cddb9559e73771fd3eef0
Shruti <sraj@renci.org>
2019-09-17 11:00:32 -0400

    test_func.py updated

--- test/test_func.py
+++ test/test_func.py
@@ -1,5 +1,5 @@
 from pdsphenotypemapping import dispatcher
 
 def test_func():
-    dispatcher.lookupFHIR("test/data", "1", [{"system":"", "code":""}])
+    dispatcher.lookupFHIR("testcases/fail", "14518", [{"system":"http://hl7.org/fhir/sid/icd-10", "code":"T36.5X5"}])
     assert True
