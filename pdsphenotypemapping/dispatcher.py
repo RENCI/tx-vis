b1047f34350f3bf587d2451727017d91db844324
Shruti <sraj@renci.org>
2019-09-19 13:42:25 -0400

    Test cases updated

--- pdsphenotypemapping/dispatcher.py
+++ pdsphenotypemapping/dispatcher.py
@@ -3,34 +3,43 @@ import os
 
 # function for loading patient record file(s)
 
-def loadFHIR(input_file): 
-    return json.load(input_file)
+def loadFHIR(input_file):
+    with open(input_file) as inp_file:
+        return json.load(inp_file)
 
 def lookupFHIR(input_dir, patient_id, codes): # 'codes' parameter is a list of [{"system":"", "code":""}]
     records = []
     for root, _, fnames in os.walk(input_dir, topdown=True):
         for f in fnames:
-            resc = loadFHIR(os.path.join(root, f))
+            resc = loadFHIR(os.path.join(root,f))
             for c in codes:  
                 system = c["system"]
                 code = c["code"]
                 if system == "http://loinc.org":
                     resc_type = "Observation"
+                    print ("Observation")
                 elif system == "http://hl7.org/fhir/sid/icd-10":
                     resc_type = "Condition"
+                    print ("Condition")
+                elif system == "http://hl7.org/fhir/sid/icd-10-cm":
+                    resc_type = "Condition"
+                    print ("Condition")
                 elif system == "http://hl7.org/fhir/sid/icd-9-cm":
                     resc_type = "Condition"
+                    print ("Condition")
                 else:
                     print ("Unknown resource type")
 
                 if resc["resourceType"] == resc_type:
+                    print ("resource type matched")
                     for c2 in resc["code"]["coding"]: # compares system and diagnosis codes
                         if c2["system"] == system and c2["code"] == code:  
-                            for p in patient_id: # compares patient_ids
-                                pid = resc["subject"]["reference"].split("/")
-                                if p == pid[1]:
-                                    records.append(resc)
-                                    return records                            
+                            print ("System and diagnosis code matched")
+                            pid = resc["subject"]["reference"].split("/") # compares patient_ids
+                            if patient_id == pid[1]:
+                                print("patient found")
+                                records.append(resc)
+    return records                            
                     
                 
     
--- test/test_func.py
+++ test/test_func.py
@@ -1,5 +1,82 @@
 from pdsphenotypemapping import dispatcher
 
-def test_func():
-    dispatcher.lookupFHIR("testcases/fail", "14518", [{"system":"http://hl7.org/fhir/sid/icd-10", "code":"T36.5X5"}])
-    assert True
+def test_func_pc1():
+
+    result=dispatcher.lookupFHIR("test/testcases/passcases/passcond1", "17944", [{"system":"http://hl7.org/fhir/sid/icd-10-cm", "code":"C50.021"}])
+    arraylen=len(result)
+    assert arraylen!=0
+def test_func_pc2():
+
+    result=dispatcher.lookupFHIR("test/testcases/passcases/passcond2", "12595", [{"system":"http://hl7.org/fhir/sid/icd-9-cm", "code":"401.9"}])
+    arraylen=len(result)
+    assert arraylen!=0
+def test_func_po1():
+
+    result=dispatcher.lookupFHIR("test/testcases/passcases/passobs1", "3533", [{"system":"http://loinc.org", "code":"7867677-4"}])
+    arraylen=len(result)
+    assert arraylen!=0
+def test_func_po2():
+
+    result=dispatcher.lookupFHIR("test/testcases/passcases/passobs2", "55708", [{"system":"http://loinc.org", "code":"785-6"}])
+    arraylen=len(result)
+    assert arraylen!=0
+def test_func_pb1():
+
+    result=dispatcher.lookupFHIR("test/testcases/passcases/passbleeding1", "13429", [{"system":"http://hl7.org/fhir/sid/icd-10", "code":"T85.830"}])
+    arraylen=len(result)
+    assert arraylen!=0
+def test_func_pb2():
+
+    result=dispatcher.lookupFHIR("test/testcases/passcases/passbleeding2", "12250", [{"system":"http://hl7.org/fhir/sid/icd-10", "code":"H31.3"}])
+    arraylen=len(result)
+    assert arraylen!=0
+def test_func_pg():
+
+    result=dispatcher.lookupFHIR("test/testcases/passcases/passpreg", "17937", [{"system":"http://hl7.org/fhir/sid/icd-10-cm", "code":"Z34.*"}])
+    arraylen=len(result)
+    assert arraylen!=0
+def test_func_pk1():
+
+    result=dispatcher.lookupFHIR("test/testcases/passcases/passkidneys1", "18044", [{"system":"http://hl7.org/fhir/sid/icd-10", "code":"E11.2"}])
+    arraylen=len(result)
+    assert arraylen!=0
+def test_func_pk2():
+
+    result=dispatcher.lookupFHIR("test/testcases/passcases/passkidneys2", "14518", [{"system":"http://hl7.org/fhir/sid/icd-10", "code":"T36.5X5"}])
+    arraylen=len(result)
+    assert arraylen!=0
+def test_func_pcc1():
+
+    result=dispatcher.lookupFHIR("test/testcases/passcases/passcreatinine1", "3530", [{"system":"http://loinc.org", "code":"2160-0"}])
+    arraylen=len(result)
+    assert arraylen!=0
+def test_func_pcc2():
+
+    result=dispatcher.lookupFHIR("test/testcases/passcases/passcreatinine2", "66", [{"system":"http://loinc.org", "code":"2160-0"}])
+    arraylen=len(result)
+    assert arraylen!=0
+def test_func_fo1():
+
+    result=dispatcher.lookupFHIR("test/testcases/passcases/failobs1", "55708", [{"system":"http://loinc.org", "code":"2160-0"}])
+    arraylen=len(result)
+    assert arraylen!=0
+def test_func_fo2():
+
+    result=dispatcher.lookupFHIR("test/testcases/passcases/failobs2", "14", [{"system":"http://loinc.org", "code":"2160-0"}])
+    arraylen=len(result)
+    assert arraylen!=0
+def test_func_fc1():
+
+    result=dispatcher.lookupFHIR("test/testcases/passcases/failcond1", "13429", [{"system":"http://loinc.org", "code":"2160-0"}])
+    arraylen=len(result)
+    assert arraylen!=0
+def test_func_fc2():
+
+    result=dispatcher.lookupFHIR("test/testcases/passcases/failcond2", "14", [{"system":"http://loinc.org", "code":"2160-0"}])
+    arraylen=len(result)
+    assert arraylen!=0
+
+
+
+
+      
