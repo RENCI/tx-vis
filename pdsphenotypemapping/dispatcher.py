4af4a75d1c81e25952f04922d24f1e18b56f77dd
Hao Xu <xuh@cs.unc.edu>
2019-11-13 13:13:59 -0500

    update

--- pdsphenotypemapping/dispatcher.py
+++ pdsphenotypemapping/dispatcher.py
@@ -3,30 +3,22 @@ import os
 import re
 from oslash import Left, Right
 from .clinical_feature import mapping
-import functools
+from tx.functional.utils import monad_utils, case, identity
 
-append = lambda l: lambda a : l + [a]
+m = monad_utils(Right)
 
-pure = lambda a: Right(a)
+sequence = m.sequence
 
-fmap = lambda f: lambda a: pure(f) * a
-
-liftA2 = lambda f: lambda a, b: fmap(f)(a) * b 
-
-sequence = lambda ms: functools.reduce(liftA2(append), ms, Right([]))
-
+def add_status_code(v):
+    if type(v) is tuple:
+        return v
+    else:
+        return v, 500
 
 # Function that returns patient's demographic and clinical feature data
 def lookupClinicals(patient_id, timestamp, data_provider_plugin_id, clinical_feature_variables_and_units):
     mapper = sequence(map(lambda x: lookupClinical(patient_id, x["clinical_feature_variable"], x.get("unit"), timestamp, data_provider_plugin_id), clinical_feature_variables_and_units))
-    if isinstance(mapper, Left):
-        v = mapper.value
-        if type(v) is tuple:
-            return v
-        else:
-            return v, 500
-    else:
-        return mapper.value
+    return case(mapper)(add_status_code)(identity)
 
 
 def lookupClinical(patient_id, clinical, unit, timestamp, data_provider_plugin_id):
